b4438b33e1d0cebcb85157237e595a3f5e986bfc
amiekisiak <117371691+amiekisiak@users.noreply.github.com>
2023-01-16 00:00:23 +0000

    added scores.js

---
+++ questions.js
@@ -0,0 +1,136 @@
+var choices = [
+
+    {
+        question: "Which of these is a valid JavaScript comment?",
+        answers: [
+            "<!-- comment goes here -->",
+            "// comment goes here",
+            "///comment goes here",
+            "::comment goes here",
+     
+       ],
+
+        correctAnswerIndex: 1,
+    },
+
+    {
+        question: "What is the preferred way to loop through an Array?",
+        answers: [
+            
+            "for loop",
+            "while loop",
+            "list loop",
+            "for ... of loop",
+        
+        ],
+
+        correctAnswerIndex: 3,
+    },
+
+    
+    {
+        question: "The Document Object Model (DOM) is a hierarchy that most closely resembles what object?",
+        answers: [
+            "a spiderweb",
+            " a castle",
+            "a tree",
+            "a tournament bracket",
+        ],
+
+        correctAnswerIndex: 2
+    },
+
+
+    {
+        question: "How can we access the data attributes of an HTML element from within JavaScript?",
+        answers: [
+            "element.dataset",
+            "element.getAttribute",
+            "element.getData()",
+            "element.fetchData()",
+        
+        ],
+        correctAnswerIndex: 0,
+    },
+
+    {
+        question: "What method allows us to add an attribute to a DOM element?",
+        answers: [
+            "element.getAttribute()",
+            "element.createAttribute()",
+            "element.setAttribute()",
+            "element.makeAttribute()",
+        
+        ],
+
+        correctAnswerIndex: 2,
+    },
+
+ 
+
+    {
+        question: "Which of the following type of variable is visible everywhere in your JavaScript code?",
+        answers: [
+            "global variable",
+            "local variable",
+            "Both of the above",
+            "None of the above.",
+        ],
+
+        correctAnswerIndex: 0,
+    },
+
+    {
+        question: "To insert a JavaScript into an HTML page, which tag is used?",
+        answers: [
+            "<script=â€™javaâ€™>",
+            "<script>",
+            "<javascript>",
+            "<js>",
+        
+        ],
+
+        correctAnswerIndex: 1,
+    },
+
+    {
+        question: "What JavaScript keyword declares a variable?",
+        answers: [
+            "if",
+            "create",
+            "var",
+            "for",
+        
+        ],
+
+        correctAnswerIndex: 2,
+    },
+
+    {
+        question: "JavaScript is interpreted by ?",
+        answers: [
+            "Client",
+            "Server",
+            "Object",
+            "None of the above",
+        
+        ],
+
+        correctAnswerIndex: 0,
+    },
+
+    {
+        question: "Using which statement is how you test for a specific condition. ",
+        answers: [
+        "Select",
+        "If",
+        "Switch",
+        "For", 
+            
+        ],
+
+        correctAnswerIndex: 1,
+    },
+
+]
+
